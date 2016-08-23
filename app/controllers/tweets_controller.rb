957478338be1c5aba01aeee279663190eb8478ec
Alexis <aklcarroll@gmail.com>
2016-08-23 01:34:20 +0000

    Set Up Read Update and Destroy Methods for Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,28 @@
 class TweetsController < ApplicationController
   before_filter :authenticate_user!
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    
   end
 
   def edit
+    
   end
-
+  
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: "Tweet Updated." }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
   def new
     @tweet = Tweet.new
   end
@@ -25,7 +38,23 @@ class TweetsController < ApplicationController
       end
     end
   end
-
-  def _form
+  
+  def destroy
+    @tweet.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: 'Your tweet has been destroyed.'}
+    end
   end
+  
+  private
+  
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+  
+  def  set_tweet
+        @tweet = Tweet.find(params[:id])
+  end
+  
 end
