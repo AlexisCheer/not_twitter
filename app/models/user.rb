52f5c4e6903e4bd75dd1937ead9480befdfb1a70
Alexis <aklcarroll@gmail.com>
2016-08-25 00:58:47 +0000

    Set up likes and tables relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   has_many :friends, through: :relationships
   has_many :inverse_relationships, class_name: "Relationship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_relationships, source: :user
+  has_many :likes
   
   validates :username, presence: true, uniqueness: true
 end
